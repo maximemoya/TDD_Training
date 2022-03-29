1ea0bd5862e1ac7e4fdaf8c1c70ea4525d44773c
maximemoya <maximemoya@gmail.com>
2022-03-29 08:50:07 +0200

    second_push

--- src/main/kotlin/FizzBuzz.kt
+++ src/main/kotlin/FizzBuzz.kt
@@ -10,11 +10,11 @@ fun fizzBuzz(minNumber: Int, maxNumber: Int): String {
                     when (i % 15) {
                         0 -> "Fizz Buzz"
                         else -> {
-                            when (i % 3) {
-                                0 -> "Fizz"
+                            when (i % 5) {
+                                0 -> "Buzz"
                                 else -> {
-                                    when (i % 5) {
-                                        0 -> "Buzz"
+                                    when (i % 3) {
+                                        0 -> "Fizz"
                                         else -> i.toString()
                                     }
                                 }
@@ -25,31 +25,6 @@ fun fizzBuzz(minNumber: Int, maxNumber: Int): String {
             }
 
         if (i != maxNumber) {
-            bufferResult += ", "
-        }
-
-    }
-    return bufferResult
-
-}
-
-fun fizzBuzzWithIf(round: Int): String {
-
-    var bufferResult = ""
-    for (i in 1..round) {
-
-        bufferResult += if (i % 15 == 0) {
-            "Fizz Buzz"
-        } else {
-            if (i % 3 == 0) {
-                "Fizz"
-            } else if (i % 5 == 0) {
-                "Buzz"
-            } else {
-                i.toString()
-            }
-        }
-        if (i != round) {
             bufferResult += ", "
         }
 
